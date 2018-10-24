ef3ddd4abce9be81ef99978e42b2f807c1396b0b
I0555 <i0555@I0555s-MacBook-Pro.local>
2018-10-24 14:08:44 +0530

    docs

--- demoq/api/controllers/views.py
+++ demoq/api/controllers/views.py
@@ -4,6 +4,22 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class DiagModelViewSet(ModelViewSet):
+    """
+    retrieve:
+    Return the given Diagnosis.
+
+    list:
+    Return a list of all the existing Diagnosis.
+
+    create:
+    Create a new Diagnosis instance.
+
+    delete:
+    Delete a Diagnosis instance.
+
+    update:
+    Update a Diagnosis instance.
+    """
     serializer_class = DiagSerializer
     queryset = Diag.objects.all()
     pagination_class = PageNumberPagination
